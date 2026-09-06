aabd7039f414f7eece9b94e95c5265807900e61e
Rajindra04 <94608821+Rajindra04@users.noreply.github.com>
2026-09-06 14:52:52 +1000

    Update ADMIN_API_BASE URL in site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,6 +1,6 @@
 (function () {
   'use strict';
-  var ADMIN_API_BASE = window.APEX_ADMIN_API_BASE || '';
+  var ADMIN_API_BASE = window.APEX_ADMIN_API_BASE = 'https://apex-bionet-admin.rajindra04.workers.dev';
   var data = null;
   var page = document.documentElement.getAttribute('data-page') || '';
 
